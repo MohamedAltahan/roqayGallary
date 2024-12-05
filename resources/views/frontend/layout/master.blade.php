855d21d57a59bd1265753ab0275178b220be932b
mohamed <medo85559@gmail.com>
2024-12-05 07:45:32 +0200

    .

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -77,7 +77,6 @@
                 font-family: "UKIJChiK" !important;
                 font-weight: 400;
                 font-style: bold !important;
-                font-size: 110%;
                 font-style: normal !important;
                 /* Changes the font color for all text */
             }
