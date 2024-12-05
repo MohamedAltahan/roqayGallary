d9f5a3f582a8d824213e35f74e4e5e404dafe50c
mohamed <medo85559@gmail.com>
2024-12-05 21:00:11 +0200

    .

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -16,6 +16,10 @@
         <!-- Template CSS -->
         <link rel="stylesheet" href="{{ asset('backend/assets/css/style.css') }}">
         <link rel="stylesheet" href="{{ asset('backend/assets/css/components.css') }}">
+        @php
+            $logo = \App\Models\LogoSetting::first();
+        @endphp
+
         <!-- Start GA -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
         <script>
@@ -39,7 +43,7 @@
                         <div
                             class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                             <div class="login-brand">
-                                <img src="{{ asset('uploads/' . @$logoSetting->icon) }}" alt="logo" width="60"
+                                <img src="{{ asset('uploads/' . @$logo->main_logo) }}" alt="logo" width="60"
                                     class="">
                             </div>
 
