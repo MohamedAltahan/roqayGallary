eb4582799b5e630b301513bb9882b321efa88319
mohamed <medo85559@gmail.com>
2024-12-06 06:06:06 +0200

    .

--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -6,10 +6,10 @@
         <div class="row">
             <div class=" col-sm-12 col-md-3">
                 <div class="list-group" id="list-tab" role="tablist">
-                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list"
-                        href="#list-home" role="tab">General Setting</a>
                     <a class="list-group-item list-group-item-action" id="list-home-page-list" data-toggle="list"
                         href="#list-home-page" role="tab">Home Page</a>
+                    <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list"
+                        href="#list-home" role="tab">General Setting</a>
                     {{-- <a class="list-group-item list-group-item-action" id="list-home-showreels-page-list" data-toggle="list"
                         href="#list-home-showreels-page" role="tab">Home Showreels & photos</a> --}}
                     <a class="list-group-item list-group-item-action" id="list-about-list" data-toggle="list"
